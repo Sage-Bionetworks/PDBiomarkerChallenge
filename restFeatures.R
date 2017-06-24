dfb98314a81668feded053f322090ba02b044c18
Yooree Chae <ychae@users.noreply.github.com>
2017-06-23 17:28:31 -0700

    Create restFeatures.R

---
+++ restFeatures.R
@@ -0,0 +1,78 @@
+# Rest feature extraction for test data: demographics:syn9819508 , table:syn9819506
+# Rest feature extraction for training data: demographics:syn9819507 , table:syn9819505
+library(synapseClient)
+synapseLogin()
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(doMC)
+library(jsonlite)
+library(parallel)
+library(tidyr)
+library(lubridate)
+library(stringr)
+library(parsedate)
+library(mpowertools) # to install mpowertools run: library(devtools); devtools::install_github("Sage-Bionetworks/mpowertools")
+
+# read in the healthCodes of interest from demographics testing table
+# for training features: change synId here to match the training demographics synId listed at top
+demo_syntable <- synTableQuery("SELECT * FROM syn9819508") 
+demo <- demo_syntable@values
+healthCodeCol <- c(as.character(demo$healthCode))
+healthCodeList <- paste0(sprintf("'%s'", healthCodeCol), collapse = ", ")
+
+# Query table of interest, walking testing table
+# for training features: change synId here to match the training table synId listed at top
+INPUT_REST_ACTIVITY_TABLE_SYNID = "syn9819506"
+actv_rest_syntable <- synTableQuery(paste0("SELECT 'recordId', 'healthCode', 'deviceMotion_walking_rest.json.items' FROM ", INPUT_REST_ACTIVITY_TABLE_SYNID, " WHERE healthCode IN ", "(", healthCodeList, ")"))
+actv_rest <- actv_rest_syntable@values
+actv_rest$idx <- rownames(actv_rest)
+
+# saving for later
+actvRest_selected_rows <- actv_rest$idx
+
+######################
+# Download JSON Files
+######################
+# rest JSON files
+rest_json_files <- synDownloadTableColumns(actv_rest_syntable, "deviceMotion_walking_rest.json.items")
+rest_json_files <- data.frame(rest_json_fileId =names(rest_json_files),
+                              rest_json_file = as.character(rest_json_files))
+actv_rest <- merge(actv_rest,rest_json_files, by.x="deviceMotion_walking_rest.json.items", by.y="rest_json_fileId", all=T)
+
+# only use the selected non-redundant data
+actv_rest <- actv_rest %>% filter(idx %in% actvRest_selected_rows)
+
+#############
+# Feature Extraction
+##############
+if (detectCores() >= 2) {
+  runParallel <- TRUE
+} else {
+  runParallel <- FALSE
+}
+registerDoMC(detectCores() - 2)
+
+# convert to character to be able to read in rest_json_file
+actv_rest[] <- lapply(actv_rest, as.character)
+
+#extract Rest features
+restFeaturesDf <- 
+  ddply(
+    .data=actv_rest,
+    .variables = colnames(actv_rest), 
+    .fun = function(row) { 
+      getRestFeatures(row$rest_json_file) 
+    }, 
+    .parallel = TRUE )
+
+# remove unnecessary columns
+columns_to_drop <- c("deviceMotion_walking_rest.json.items",
+                     "idx",
+                     "rest_json_file",
+                     "error")
+cols_to_keep <- !colnames(restFeaturesDf) %in% columns_to_drop
+restFeatures <- restFeaturesDf[, cols_to_keep]
+
+# view the data
+View(restFeatures)
